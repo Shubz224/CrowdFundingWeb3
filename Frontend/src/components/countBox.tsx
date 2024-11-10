89bad0f426e6d2b528107dadcb86994627a7ccc0
Shubham Nikam <nikamshubham224@gmail.com>
2024-11-10 15:41:34 +0530

    countbox err

--- Frontend/src/components/countBox.tsx
+++ Frontend/src/components/countBox.tsx
@@ -10,13 +10,13 @@ export function CountBox({
 }: CountBoxProps) {
     return (
         <div className="flex flex-col items-center w-[150px]">
-            <h4 className="font-epilogue font-bold text-[30px] text-white p-3 bg-[#1c1c24]
+            <h4 className="font-epilogue font-bold text-[30px] text-black p-3 bg-[#ECF1FF]
              rounded-t-[10px] w-full text-center truncate"
             >
                 {value}
             </h4>
             <p className="font-epilogue font-normal text-[16px] text-[#808091]
-             bg-[#28282e] px-3 py-2 w-full rounded-b-[10px] text-center"
+             bg-[#ECF1FF] px-3 py-2 w-full rounded-b-[10px] text-center"
             >
                 {title}
             </p>
