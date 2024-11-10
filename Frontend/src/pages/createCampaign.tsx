6bd2893438cdb463517236ee6c326246e86e9d53
Shubham Nikam <nikamshubham224@gmail.com>
2024-11-10 15:44:27 +0530

    loader error and removed title

--- Frontend/src/pages/createCampaign.tsx
+++ Frontend/src/pages/createCampaign.tsx
@@ -45,14 +45,14 @@ export function CreateCampaign() {
     }
 
     return (
-        <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+        <div className="bg-[#E8F0FE] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
             {isLoading && <Loader />}
-            <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+            <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#1872E8] rounded-[10px]">
                 <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">Start a Campaign</h1>
             </div>
 
-            <form onSubmit={handleSubmit} className="w-full mt-[65px] flex flex-col gap-[30px]">
-                <div className="flex flex-wrap gap-[40px]">
+            <form onSubmit={handleSubmit} className="w-full mt-[65px] flex flex-col gap-[30px]  colour-black">
+                <div className="flex flex-wrap gap-[40px] text-black ">
                     <FormField
                         labelName="Your Name *"
                         placeholder="John Doe"
@@ -76,12 +76,7 @@ export function CreateCampaign() {
                     handleChange={(e) => handleFormFieldChange('description', e)}
                 />
 
-                <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]">
-                    <img src={money} alt="money" className="w-[40px] h-[40px] object-contain" />
-                    <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">
-                        You will get 100% of the raised amount
-                    </h4>
-                </div>
+               
 
                 <div className="flex flex-wrap gap-[40px]">
                     <FormField
@@ -107,11 +102,11 @@ export function CreateCampaign() {
                     handleChange={(e) => handleFormFieldChange('image', e)}
                 />
 
-                <div className="flex justify-center items-center mt-[40px]">
+                <div className="flex justify-center items-center mt-[40px] ">
                     <CustomButton
                         btnType="submit"
                         title="Submit new campaign"
-                        styles="bg-[#1dc071]"
+                        styles="bg-[#1dc071] text-white"
                     />
                 </div>
             </form>
