853564261472b75818be728c9d7494a123d6a506
Shubham Nikam <nikamshubham224@gmail.com>
2025-03-18 03:59:34 +0530

    chain link changed

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,19 +1,22 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Sepolia } from '@thirdweb-dev/chains'
+import { ArbitrumSepolia } from '@thirdweb-dev/chains'
 import { ThirdwebProvider } from '@thirdweb-dev/react'
 import { App } from './app'
 import './index.css'
 import { Toaster } from 'sonner'
 import { StateContextProvider } from './contexts'
-
+import{AuthProvider}from "./utils/AuthContext"
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-    <ThirdwebProvider activeChain={Sepolia} clientId={"2a647bda5cc44525bffd8c11c018f655"}>
+    <ThirdwebProvider activeChain={ArbitrumSepolia} clientId={"1b375590eca360b28c55ce2bdf51b0f9"}>
         <Router>
             <StateContextProvider>
+                <AuthProvider>
                 <App />
+                </AuthProvider>
+                
             </StateContextProvider>
         </Router>
         <Toaster richColors />
