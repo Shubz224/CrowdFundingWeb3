79afaa8cf1c4724adfd9e4631dfe91084309abef
Shubham Nikam <nikamshubham224@gmail.com>
2025-03-18 03:59:20 +0530

    Login error solved

--- Frontend/src/app.tsx
+++ Frontend/src/app.tsx
@@ -5,6 +5,10 @@ import { Navbar } from "./components/navbar";
 import { CreateCampaign } from "./pages/createCampaign";
 import { Profile } from "./pages/profile";
 import { CampaignDetails } from "./pages/campaignDetails";
+import { AdminDashboard } from "./pages/admin";
+import { useAuth } from "./utils/AuthContext";
+import ProtectedRoute from '../auth/ProtectedRoute'
+import { Login } from './pages/login'
 
 export const App = () => {
   return (
@@ -15,10 +19,14 @@ export const App = () => {
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         <Navbar />
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/create-campaign" element={
+            <CreateCampaign />} />
+
           <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+          <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </div>
     </div>
