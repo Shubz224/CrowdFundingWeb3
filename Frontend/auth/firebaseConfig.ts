0957852007288d2a0460bf4af8a5761deac746e4
Shubham Nikam <nikamshubham224@gmail.com>
2025-03-18 04:02:22 +0530

    key removed

--- Frontend/auth/firebaseConfig.ts
+++ Frontend/auth/firebaseConfig.ts
@@ -4,13 +4,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIvTPaf76N54885xjvene531wgBW2-AAQ",
-  authDomain: "crowdfunding-d093e.firebaseapp.com",
-  projectId: "crowdfunding-d093e",
-  storageBucket: "crowdfunding-d093e.firebasestorage.app",
-  messagingSenderId: "246319464084",
-  appId: "1:246319464084:web:7c928c9a0074c215d1fe81",
-  measurementId: "G-86Q51BBSQD"
+  
 };
 
 const app = initializeApp(firebaseConfig);
